85bb0f6f009dfae28adf665b9cf7b5c847719299
Daniel Lee <lee@isi-solutions.org>
2015-10-23 20:24:11 +0200

    Fix syntax error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-with open('README.md')) as readme:
+with open('README.md') as readme:
     README = readme.read()
 
 setup(
